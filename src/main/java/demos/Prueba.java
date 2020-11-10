5dd630eb0929f87a6ae424023945524b1802e091
begonaolea <begonaolea@192.168.1.8>
2020-11-10 09:32:05 +0100

    Pruebas atributos y var locales

--- src/main/java/demos/Prueba.java
+++ src/main/java/demos/Prueba.java
@@ -14,10 +14,10 @@ public class Prueba {
         String s  = null;
         int longitud = s.length();
         
-        
         Prueba p = new Prueba(); 
         //crear un objeto Prueba con un atributo nombre = null
         longitud = p.nombre.length();
+        
         
     }
 }

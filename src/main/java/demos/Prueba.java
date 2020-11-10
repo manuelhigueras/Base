08ee002bb3d42869f88840f3cea99b1557a73b58
begonaolea <begonaolea@192.168.1.8>
2020-11-10 09:26:31 +0100

    Pruebas atributos y var locales

--- src/main/java/demos/Prueba.java
+++ src/main/java/demos/Prueba.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package demos;
 
 /**
@@ -10,5 +6,18 @@ package demos;
  * @author begonaolea
  */
 public class Prueba {
+    //atributos
+    String nombre;
     
+    public static void main(String[] args) {
+        //var locales
+        String s  = null;
+        int longitud = s.length();
+        
+        
+        Prueba p = new Prueba(); 
+        //crear un objeto Prueba con un atributo nombre = null
+        longitud = p.nombre.length();
+        
+    }
 }

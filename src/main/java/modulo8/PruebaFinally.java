bdedc45106c4bd8ae4dc9acec155cef6defe27dc
begonaolea <begonaolea@192.168.1.8>
2020-11-17 10:13:41 +0100

    FINALLY

--- src/main/java/modulo8/PruebaFinally.java
+++ src/main/java/modulo8/PruebaFinally.java
@@ -1,24 +1,25 @@
-
 package modulo8;
 
-
 public class PruebaFinally {
-    
-    
+
     public static void main(String[] args) {
-        suma("SUMATORIO", 3,4,6,7,8,67);
-    }
-    
-    
-    public static void suma(String titulo, int... numeros){
+        suma("SUMATORIO", 3, 4, 6, 7, 8, 67);
+        suma(null, 34, 56, 7);
+    }//fin main
+
+    public static void suma(String titulo, int... numeros) {
         int total = 0;
-        for(int n : numeros){
+        for (int n : numeros) {
             total += n;
         }
-        System.out.println(titulo.toUpperCase()  );
-        System.out.println("...................");
-        System.out.println("Total : " + total);
-        
+        try {
+            System.out.println(titulo.toUpperCase());
+            System.out.println("...................");
+            System.out.println("Total : " + total);
+        } catch (Exception e) {
+            System.out.println("Has indicado mas el titulo");
+        }finally{
+            System.out.println(".... fin  suma");
+        }
     }//suma
-    
-}
+}//fin clase
